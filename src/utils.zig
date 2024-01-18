1b558bf0a6dd2f77285b3f3e944bd95c095a9084
Sören Tempel <soeren+git@soeren-tempel.net>
2024-01-19 00:04:00 +0100

    util: remove unused function

--- src/utils.zig
+++ src/utils.zig
@@ -5,14 +5,6 @@ const meta = std.meta;
 const os = std.os;
 const unicode = std.unicode;
 
-pub fn cast(comptime to: type) fn (*anyopaque) *to {
-    return (struct {
-        pub fn cast(module: *anyopaque) *to {
-            return @ptrCast(*to, @alignCast(@alignOf(to), module));
-        }
-    }).cast;
-}
-
 pub fn toUtf8(gpa: mem.Allocator, bytes: []const u8) ![]u32 {
     const utf8 = try unicode.Utf8View.init(bytes);
     var iter = utf8.iterator();
