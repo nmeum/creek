e02cabbf089004071777845b06e85919158dc390
Sören Tempel <soeren+git@soeren-tempel.net>
2024-03-14 05:17:12 +0100

    Seat: Refactor event handling

--- src/Seat.zig
+++ src/Seat.zig
@@ -77,93 +77,99 @@ fn updateTitle(self: *Seat, data: [*:0]const u8) void {
     }
 }
 
+fn focusedOutput(self: *Seat, output: *wl.Output) void {
+    var monitor: ?*Monitor = null;
+    for (state.wayland.monitors.items) |m| {
+        if (m.output == output) {
+            monitor = m;
+            break;
+        }
+    }
+
+    if (monitor) |m| {
+        if (m.bar) |bar| {
+            if (bar.configured) {
+                self.current_output = m.output;
+                render.renderText(bar, self.status_text) catch |err| {
+                    log.err("renderText failed for monitor {}: {s}",
+                        .{m.globalName, @errorName(err)});
+                    return;
+                };
+
+                bar.text.surface.commit();
+                bar.background.surface.commit();
+            }
+        }
+    } else {
+        log.err("seatListener: couldn't find focused output", .{});
+    }
+}
+
+fn unfocusedOutput(self: *Seat, output: *wl.Output) void {
+    var monitor: ?*Monitor = null;
+    for (state.wayland.monitors.items) |m| {
+        if (m.output == output) {
+            monitor = m;
+            break;
+        }
+    }
+
+    if (monitor) |m| {
+        // TODO: add getBar or something
+        if (m.bar) |bar| {
+            if (bar.configured) {
+                render.resetText(bar) catch |err| {
+                    log.err("renderTitle failed for monitor {}: {s}",
+                        .{bar.monitor.globalName, @errorName(err)});
+
+                    };
+                bar.text.surface.commit();
+
+                render.renderTitle(bar, null) catch |err| {
+                    log.err("renderTitle failed for monitor {}: {s}",
+                        .{bar.monitor.globalName, @errorName(err)});
+                    return;
+                };
+
+                bar.title.surface.commit();
+                bar.background.surface.commit();
+            }
+        }
+    } else {
+        log.err("seatListener: couldn't find unfocused output", .{});
+    }
+
+    self.current_output = null;
+}
+
+fn focusedView(self: *Seat, title: [*:0]const u8) void {
+    self.updateTitle(title);
+    if (self.focusedMonitor()) |monitor| {
+        if (monitor.bar) |bar| {
+            if (!bar.configured) {
+                return;
+            }
+
+            render.renderTitle(bar, self.window_title) catch |err| {
+                log.err("renderTitle failed for monitor {}: {s}",
+                    .{bar.monitor.globalName, @errorName(err)});
+                return;
+            };
+
+            bar.title.surface.commit();
+            bar.background.surface.commit();
+        }
+    }
+}
+
 fn seatListener(
     _: *zriver.SeatStatusV1,
     event: zriver.SeatStatusV1.Event,
     seat: *Seat,
 ) void {
     switch (event) {
-        .focused_output => |data| {
-            var monitor: ?*Monitor = null;
-            for (state.wayland.monitors.items) |m| {
-                if (m.output == data.output) {
-                    monitor = m;
-                    break;
-                }
-            }
-
-            if (monitor) |m| {
-                if (m.bar) |bar| {
-                    if (bar.configured) {
-                        seat.current_output = m.output;
-                        render.renderText(bar, seat.status_text) catch |err| {
-                            log.err("renderText failed for monitor {}: {s}",
-                                .{m.globalName, @errorName(err)});
-                            return;
-                        };
-
-                        bar.text.surface.commit();
-                        bar.background.surface.commit();
-                    }
-                }
-            } else {
-                log.err("seatListener: couldn't find focused output", .{});
-            }
-        },
-        .unfocused_output => |data| {
-            var monitor: ?*Monitor = null;
-            for (state.wayland.monitors.items) |m| {
-                if (m.output == data.output) {
-                    monitor = m;
-                    break;
-                }
-            }
-
-            if (monitor) |m| {
-                // TODO: add getBar or something
-                if (m.bar) |bar| {
-                    if (bar.configured) {
-                        render.resetText(bar) catch |err| {
-                            log.err("renderTitle failed for monitor {}: {s}",
-                                .{bar.monitor.globalName, @errorName(err)});
-
-                        };
-                        bar.text.surface.commit();
-
-                        render.renderTitle(bar, null) catch |err| {
-                            log.err("renderTitle failed for monitor {}: {s}",
-                                .{bar.monitor.globalName, @errorName(err)});
-                            return;
-                        };
-
-                        bar.title.surface.commit();
-                        bar.background.surface.commit();
-                    }
-                }
-            } else {
-                log.err("seatListener: couldn't find unfocused output", .{});
-            }
-
-            seat.current_output = null;
-        },
-        .focused_view => |data| {
-            seat.updateTitle(data.title);
-            if (seat.focusedMonitor()) |monitor| {
-                if (monitor.bar) |bar| {
-                    if (!bar.configured) {
-                        return;
-                    }
-
-                    render.renderTitle(bar, seat.window_title) catch |err| {
-                        log.err("renderTitle failed for monitor {}: {s}",
-                               .{bar.monitor.globalName, @errorName(err)});
-                        return;
-                    };
-
-                    bar.title.surface.commit();
-                    bar.background.surface.commit();
-                }
-            }
-        },
+        .focused_output => |data| seat.focusedOutput(data.output.?),
+        .unfocused_output => |data| seat.unfocusedOutput(data.output.?),
+        .focused_view => |data| seat.focusedView(data.title),
     }
 }
